6315e2dc60c2f44f62f5e66b5d8831c0a2de2be3
joe driver <joewdriver@gmail.com>
2016-09-14 22:49:13 -0400

    initial commit for mpty project

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/joewd_000/flashcards/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.joewd_000.flashcards.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.joewd_000.flashcards.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/src/main/java/com/example/joewd_000/flashcards/FlashCards.java
@@ -0,0 +1,13 @@
+package com.example.joewd_000.flashcards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlashCards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_cards);
+    }
+}
